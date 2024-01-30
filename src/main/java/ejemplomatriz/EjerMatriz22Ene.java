b82294e33be81053b5f2e4ae1515a386f30bc9a4
None083 <perezgonzaleznoelia08@gmail.com>
2024-01-30 11:10:44 +0100

    a estudia

---
+++ src/main/java/ejemplomatriz/EjerMatriz22Ene.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplomatriz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author noelia
+ */
+public class EjerMatriz22Ene {
+
+    /**
+     * Realiza un programa que pida al usuario el número de filas y columnas 
+     * de un array bidimensional de números enteros. El programa crea la matriz 
+     * con esos tamaños y debe rellenarla solicitando al usuario los valores 
+     * a introducir. Una vez esté rellena, se debe mostrar la matriz completa 
+     * y la suma de sus elementos.
+     * 
+     * 
+     * 23 Enero 
+     * 1. Método que recibe una matriz y la muestra en forma tabulada (tabla)
+       2. Recibe la matriz y un número de filas y devuelve la suma de esa fila
+       3. Recibe la matriz y un número de columnas y devuelve la suma de esa columna
+
+       Para cualquier tipo de matriz, no 2x2
+     * 
+     * 
+     * 
+     * 25 enero
+     * En una clase con el main, usando métodos de clase a) Método que recibe 
+     * un número "n" entre 1 y 10 y devuelve una matriz de nXn elementos aleatorios 
+     * de tipo char. b) Método que recibe la matriz y un char y devuelve una 
+     * lista de objetos Celda indicando las casillas donde se encuentra ese 
+     * char dentro de la matriz.
+     * 
+     * 
+     * 
+     * 26 Enero
+     * Autobus
+     * 12 filas 4 asientos
+     * Hacer programa para reservar y liberar asientos
+     * Si el viajero tiene movilidad reducida hay que 
+     * dejar libres todos los asientos de alrededor
+     * 
+     * habra clase autobus con una matriz, clase asiento
+     * 
+     * fila de en medio pasillo?
+     * 
+     * 
+     */
+    public static void main(String[] args) {
+        
+//        Scanner teclado = new Scanner(System.in);
+//        
+//        System.out.println("Introduce un número de filas");
+//        int numFilas = teclado.nextInt();
+//        System.out.println("Introduce un número de columnas");
+//        int numColumnas = teclado.nextInt();
+//        
+//        for (int i = 0; i < numFilas; i++) {
+//            for (int j = 0; j < 10; j++) {
+//                
+//            }
+//        }
+        
+        
+    }
+    
+}
