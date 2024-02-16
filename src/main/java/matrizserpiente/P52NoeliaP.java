f0c4f305e84923a2a8d8964a8279d47937d8154d
noelia <perezgonzaleznoelia08@gmail.com>
2024-02-16 13:20:16 +0100

    cambiado nombre paquete

--- src/main/java/matrizserpiente/P52NoeliaP.java
+++ src/main/java/matrizserpiente/P52NoeliaP.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package daw;
+package matrizserpiente;
 
 import java.util.ArrayList;
 import java.util.List;
